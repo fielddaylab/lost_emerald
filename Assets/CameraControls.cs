8bf0d0c50059bd8c2883804106a4cd7fa8074236
Michael Tolly <miketolly@gmail.com>
2020-10-21 18:28:55 -0500

    Smooth animation for camera angle switch

--- Assets/CameraControls.cs
+++ Assets/CameraControls.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BeauRoutine;
 
 public class CameraControls : MonoBehaviour
 {
@@ -8,53 +9,72 @@ public class CameraControls : MonoBehaviour
     public Vector3 topOffset;
     public Vector3 sideOffset;
 
-    bool lookingTop;
+    enum CameraState
+    {
+        CAMERA_TOP,
+        CAMERA_TOP_TO_SIDE,
+        CAMERA_SIDE,
+        CAMERA_SIDE_TO_TOP,
+    }
+
+    CameraState cameraState = CameraState.CAMERA_TOP;
     float sideAngle = 0.0f; // degrees
 
     // Start is called before the first frame update
     void Start()
     {
-        ViewTop();
+        cameraState = CameraState.CAMERA_TOP;
+        MoveCamera(1.0f);
     }
 
-    void ViewTop()
+    void MoveCamera(float percentTop)
     {
-        lookingTop = true;
-        transform.position = target.transform.position + topOffset;
-        transform.LookAt(target.transform);
-    }
-
-    void ViewSide()
-    {
-        lookingTop = false;
+        Vector3 topPosition = target.transform.position + topOffset;
         Vector3 rotatedOffset = Quaternion.Euler(0.0f, sideAngle, 0.0f) * sideOffset;
-        transform.position = target.transform.position + rotatedOffset;
-        transform.LookAt(target.transform);
+        Vector3 sidePosition = target.transform.position + rotatedOffset;
+        Vector3 animPosition = sidePosition + (topPosition - sidePosition) * percentTop;
+
+        Vector3 topUp = new Vector3(1.0f, 0.0f, 1.0f).normalized;
+        Vector3 sideUp = new Vector3(0.0f, 1.0f, 0.0f).normalized;
+        Vector3 animUp = sideUp + (topUp - sideUp) * percentTop;
+
+        transform.position = animPosition;
+        transform.LookAt(target.transform, animUp);
+    }
+
+    IEnumerator AnimateCamera(CameraState animState, float startValue, float endValue, CameraState endState)
+    {
+        cameraState = animState;
+        yield return Tween.Float(startValue, endValue, (f) => { MoveCamera(f); }, 1.0f).Ease(Curve.CubeInOut);
+        cameraState = endState;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (cameraState == CameraState.CAMERA_TOP || cameraState == CameraState.CAMERA_SIDE)
         {
-            if (lookingTop)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                ViewSide();
+                if (cameraState == CameraState.CAMERA_TOP)
+                {
+                    Routine.Start(this, AnimateCamera(CameraState.CAMERA_TOP_TO_SIDE, 1.0f, 0.0f, CameraState.CAMERA_SIDE));
+                }
+                else
+                {
+                    Routine.Start(this, AnimateCamera(CameraState.CAMERA_SIDE_TO_TOP, 0.0f, 1.0f, CameraState.CAMERA_TOP));
+                }
             }
-            else
+            else if (cameraState == CameraState.CAMERA_SIDE && Input.GetKey(KeyCode.LeftArrow))
             {
-                ViewTop();
+                sideAngle += 1.0f;
+                MoveCamera(0.0f);
             }
-        }
-        else if (!lookingTop && Input.GetKey(KeyCode.LeftArrow))
-        {
-            sideAngle += 1.0f;
-            ViewSide();
-        }
-        else if (!lookingTop && Input.GetKey(KeyCode.RightArrow))
-        {
-            sideAngle -= 1.0f;
-            ViewSide();
+            else if (cameraState == CameraState.CAMERA_SIDE && Input.GetKey(KeyCode.RightArrow))
+            {
+                sideAngle -= 1.0f;
+                MoveCamera(0.0f);
+            }
         }
     }
 }
