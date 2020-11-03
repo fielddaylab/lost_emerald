9cf2fd2015d7ea090149cdc74cee8089f0d22f4d
Michael Tolly <miketolly@gmail.com>
2020-11-02 21:25:55 -0600

    Pill shape camera track

--- Assets/CameraControls.cs
+++ Assets/CameraControls.cs
@@ -8,6 +8,9 @@ public class CameraControls : MonoBehaviour
     public GameObject target;
     public Vector3 topOffset;
     public Vector3 sideOffset;
+    public Vector3 topUpVector;
+    public Vector3 sideUpVector;
+    public float shipLength;
 
     enum CameraState
     {
@@ -18,28 +21,73 @@ public class CameraControls : MonoBehaviour
     }
 
     CameraState cameraState = CameraState.CAMERA_TOP;
-    float sideAngle = 0.0f; // degrees
+    float sideAngle = 0.0f;
+    // each semicircle circumference is pi * |sideOffset|
+    // sideAngle from 0 to shipLength: first side of ship length (L to R)
+    // sideAngle from shipLength to (shipLength + semicircle): right semicircle
+    // sideAngle from (shipLength + semicircle) to (2 * shipLength + semicircle): second side of ship length
+    // sideAngle from (2 * shipLength + semicircle) to (2 * shipLength + 2 * semicircle): left semicircle
+    // otherwise, normalize to between 0 and (2 * shipLength + 2 * semicircle)
 
     // Start is called before the first frame update
     void Start()
     {
         cameraState = CameraState.CAMERA_TOP;
+        sideAngle = shipLength / 2.0f; // so we start in the middle of the side
         MoveCamera(1.0f);
     }
 
     void MoveCamera(float percentTop)
     {
         Vector3 topPosition = target.transform.position + topOffset;
-        Vector3 rotatedOffset = Quaternion.Euler(0.0f, sideAngle, 0.0f) * sideOffset;
-        Vector3 sidePosition = target.transform.position + rotatedOffset;
-        Vector3 animPosition = sidePosition + (topPosition - sidePosition) * percentTop;
+        Vector3 topLookAt = target.transform.position;
 
-        Vector3 topUp = new Vector3(1.0f, 0.0f, 1.0f).normalized;
-        Vector3 sideUp = new Vector3(0.0f, 1.0f, 0.0f).normalized;
-        Vector3 animUp = sideUp + (topUp - sideUp) * percentTop;
+        float semiCircumference = Mathf.PI * sideOffset.magnitude;
+        float fullTrackLength = 2 * shipLength + 2 * semiCircumference;
+        float sideAngleNorm = sideAngle % fullTrackLength;
+        if (sideAngleNorm < 0)
+        {
+            sideAngleNorm += fullTrackLength;
+        }
+
+        Vector3 sidePosition = target.transform.position + sideOffset; // overwritten
+        Vector3 sideLookAt = target.transform.position; // overwritten
+        // TODO get the angle for these by rotating sideOffset around sideUpVector
+        Vector3 shipEndL = target.transform.position + new Vector3(shipLength * 0.5f, 0.0f, 0.0f);
+        Vector3 shipEndR = target.transform.position + new Vector3(shipLength * -0.5f, 0.0f, 0.0f);
+        if (0 <= sideAngleNorm && sideAngleNorm < shipLength)
+        {
+            // first side of ship length (L to R)
+            sideLookAt = shipEndL + (shipEndR - shipEndL) * (sideAngleNorm / shipLength);
+            sidePosition = sideLookAt + sideOffset;
+        }
+        else if (shipLength <= sideAngleNorm && sideAngleNorm < shipLength + semiCircumference)
+        {
+            // right semicircle
+            sideLookAt = shipEndR;
+            float degrees = ((sideAngleNorm - shipLength) / semiCircumference) * 180f;
+            sidePosition = sideLookAt + Quaternion.Euler(0f, -degrees, 0f) * sideOffset;
+        }
+        else if (shipLength + semiCircumference <= sideAngleNorm && sideAngleNorm < 2 * shipLength + semiCircumference)
+        {
+            // second side of ship length
+            sideLookAt = shipEndR + (shipEndL - shipEndR) * ((sideAngleNorm - (shipLength + semiCircumference)) / shipLength);
+            sidePosition = sideLookAt + Quaternion.Euler(0f, 180f, 0f) * sideOffset;
+        }
+        else
+        {
+            // left semicircle
+            sideLookAt = shipEndL;
+            float degrees = ((sideAngleNorm - (2 * shipLength + semiCircumference)) / semiCircumference) * 180f;
+            sidePosition = sideLookAt + Quaternion.Euler(0f, 180f - degrees, 0f) * sideOffset;
+        }
+
+        Vector3 animPosition = sidePosition + (topPosition - sidePosition) * percentTop;
+        Vector3 animUp = sideUpVector + (topUpVector - sideUpVector) * percentTop;
+        Vector3 animLookAt = sideLookAt + (topLookAt - sideLookAt) * percentTop;
 
         transform.position = animPosition;
-        transform.LookAt(target.transform, animUp);
+        transform.LookAt(animLookAt, animUp);
     }
 
     IEnumerator AnimateCamera(CameraState animState, float startValue, float endValue, CameraState endState)
@@ -67,13 +115,17 @@ public class CameraControls : MonoBehaviour
             }
             else if (cameraState == CameraState.CAMERA_SIDE && Input.GetKey(KeyCode.LeftArrow))
             {
-                sideAngle += 1.0f;
+                sideAngle -= 0.15f; // TODO do this based on actual time, not frames
                 MoveCamera(0.0f);
             }
             else if (cameraState == CameraState.CAMERA_SIDE && Input.GetKey(KeyCode.RightArrow))
             {
-                sideAngle -= 1.0f;
+                sideAngle += 0.15f; // TODO do this based on actual time, not frames
                 MoveCamera(0.0f);
+            }
+            else
+            {
+                MoveCamera(cameraState == CameraState.CAMERA_TOP ? 1.0f : 0.0f);
             }
         }
     }
