27b59c5b9ccecefa5e930ea01b5c8a0bc4efd6b5
IshitaDhoot <idhoot@wisc.edu>
2021-01-12 00:50:58 +0530

    Added Ship Mechanics

---
+++ Assets/Dive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dive : MonoBehaviour
+{
+    [SerializeField]private Image background;
+    [SerializeField] private Text dive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        background.enabled = false;
+        dive.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ship.count > 85)
+        {
+            background.enabled = true;
+            dive.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    private Vector3 dragOrigin;
+    private float step;
+    public static int count = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        step = Time.deltaTime / Mathf.PI;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            dragOrigin = Input.mousePosition;
+            return;
+        }
+
+        if (!Input.GetMouseButton(0)) return;
+
+        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        Vector3 move = new Vector3(pos.x * 30, pos.y * 30, 0);
+        transform.Translate(move, Space.World);
+        transform.rotation = Quaternion.LookRotation(transform.forward, pos);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "MaskingBlock")
+        {
+            Destroy(other.gameObject);
+            count++;
+        }
+    }
+}
