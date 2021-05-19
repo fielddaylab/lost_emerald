a58401a30ec9b7b326d91fe4eabe2782913a27f5
Terra Lauterbach <terra.lauterbach@gmail.com>
2021-05-19 15:39:21 -0500

    added base input system

---
+++ Assets/_Code/Shipwreck/EvidenceBoard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Shipwreck {
+
+
+	public class EvidenceBoard : MonoBehaviour {
+
+		
+
+	}
+
+
+}
+
+
