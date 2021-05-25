26a0504e933365e8b663b8a58a222f9a5cea85a7
Terra Lauterbach <terra.lauterbach@gmail.com>
2021-05-25 18:36:23 -0500

    added drop zones and prefabs, connected ship cards to known wrecks card

---
+++ Assets/_Code/Shipwreck/EvidenceBoard/DropZone.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Shipwreck {
+
+	public class DropZone : MonoBehaviour {
+
+		[SerializeField]
+		private Transform[] m_dropPoints;
+
+		public Transform GetClosestDropPoint(Vector3 point) {
+			float distance = float.MaxValue;
+			Transform result = null;
+			for (int ix = 0; ix < m_dropPoints.Length; ix++) {
+				float newDist = Vector3.Distance(point, m_dropPoints[ix].position);
+				if (newDist < distance) {
+					distance = newDist;
+					result = m_dropPoints[ix];
+				}
+			}
+			return result;
+		}
+
+
+	}
+
+}
+
+
