7c537ed07cdbf9df1cf2aa979287dc54d99f017d
Forest San Filippo <freakyforest@gmail.com>
2021-07-05 17:23:25 -0500

    First Pass On Virtual Camera Switching
    - Added DiveCameraControl script with basic functionality to change virtual camera priority
    - Created DiveVirtualCamera prefab with rough setup for future vCam positions
    - Updated Ship01 scene with a prefab instance

---
+++ Assets/_Code/DiveCameraControl.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+
+public class DiveCameraControl : MonoBehaviour
+{
+    public CinemachineVirtualCamera thisVCam;
+    public Collider clickTarget;
+    private Camera mainCamera;
+    private CinemachineVirtualCamera[] allVCams;
+
+    void Awake()
+	{
+        mainCamera = Camera.main;
+        allVCams = FindObjectsOfType<CinemachineVirtualCamera>();
+
+    }
+
+	void Update()
+    {
+        var currentMouse = Mouse.current;
+        if (currentMouse == null)
+            return;
+
+        if (currentMouse.leftButton.wasPressedThisFrame)
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+			{
+                if (hit.collider == clickTarget)
+				{
+                    switchToThisCam();
+				}
+			}
+
+        }
+
+    }
+
+    void switchToThisCam()
+	{
+        thisVCam.Priority = 10;
+        foreach (CinemachineVirtualCamera vCam in allVCams)
+        {
+            if (vCam != thisVCam)
+			{
+                vCam.Priority = 0;
+            }
+        }
+    }
+}
