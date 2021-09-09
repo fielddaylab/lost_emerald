2c82b119d1225c4c13cf155f8f0c1d408e14cce6
Terra Lauterbach <terra.lauterbach@gmail.com>
2021-09-09 12:51:35 -0500

    updated unity packages, evidence root now changes name based on the wreck/level you are working on

---
+++ Assets/_Code/EvidenceBoard/EvidenceRootLabel.cs
@@ -0,0 +1,39 @@
+﻿using BeauRoutine;
+using BeauUtil;
+using PotatoLocalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Shipwreck {
+
+	
+	public class EvidenceRootLabel : MonoBehaviour {
+
+		[SerializeField]
+		private LocalizedTextUGUI m_label = null;
+
+
+		private void OnEnable() {
+			GameMgr.Events.Register<StringHash32>(GameEvents.ChainSolved, HandleChainComplete);
+			RefreshLabel();
+		}
+		private void OnDisable() {
+			GameMgr.Events.Deregister<StringHash32>(GameEvents.ChainSolved, HandleChainComplete);
+		}
+
+		private void HandleChainComplete(StringHash32 root) {
+			RefreshLabel();
+		}
+
+		private void RefreshLabel() {
+			m_label.Key = GameMgr.State.CurrentLevel.Name;
+		}
+
+	}
+
+
+
+}
+
