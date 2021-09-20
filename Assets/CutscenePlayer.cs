007bc527d279f1a2c57269dc5bc9a3f0f4ee0903
Levi Huillet <levimhuillet@gmail.com>
2021-09-20 10:44:44 -0500

    modified dive scene trigger, removed redundant audio listener during cutscene

--- Assets/CutscenePlayer.cs
+++ Assets/CutscenePlayer.cs
@@ -15,6 +15,8 @@ namespace Shipwreck {
 		private VideoPlayer m_videoPlayer = null;
 		[SerializeField]
 		private Camera m_videoCamera = null;
+		[SerializeField]
+		private Camera m_gameCamera;
 
 #if UNITY_EDITOR
 		private const string VIDEO_PATH = "file://{0}/cutscene{1:00}.mp4";
@@ -26,6 +28,7 @@ namespace Shipwreck {
 			I.m_videoPlayer.url = string.Format(VIDEO_PATH, Application.streamingAssetsPath, GameMgr.State.CurrentLevel.Index + 1);
 			I.m_videoPlayer.Play();
 			I.m_videoCamera.gameObject.SetActive(true);
+			I.m_gameCamera.GetComponent<AudioListener>().enabled = false;
 		}
 
 		private void OnEnable() {
@@ -36,6 +39,7 @@ namespace Shipwreck {
 		}
 		private void HandleVideoComplete(VideoPlayer player) {
 			m_videoCamera.gameObject.SetActive(false);
+			I.m_gameCamera.GetComponent<AudioListener>().enabled = true;
 			OnVideoComplete?.Invoke();
 		}
 	}
