5861e1df3777ceda210b5a5ead211c53b0f2358c
Terra Lauterbach <terra.lauterbach@gmail.com>
2021-10-28 13:44:29 -0500

    potential fix for sticky notes appearing off screen

--- Assets/_Code/EvidenceBoard/EvidenceChain.cs
+++ Assets/_Code/EvidenceBoard/EvidenceChain.cs
@@ -131,9 +131,9 @@ namespace Shipwreck {
 			for (int index = 0; index < m_evidencePins.Length; index++) {
 				// place the note on the last active index
 				if (!m_evidencePins[index].gameObject.activeInHierarchy) {
-
-					Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, m_evidencePins[index - 1].RectTransform.position);
-					screenPoint.y = Mathf.Max(100f, screenPoint.y);
+					EvidencePin pin = m_evidencePins[index - 1];
+					Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(Camera.main, pin.RectTransform.position);
+					screenPoint.y = Mathf.Max(100f * pin.GetComponentInParent<Canvas>().scaleFactor, screenPoint.y);
 					RectTransformUtility.ScreenPointToLocalPointInRectangle(
 						(RectTransform)m_stickyNote.RectTransform.parent,
 						screenPoint,
