2e094b5dac4aedfd65272675a17950c33666a33b
Levi Huillet <levimhuillet@gmail.com>
2021-11-09 12:15:31 -0600

    implemented new live map graphics

---
+++ Assets/_Code/UI/Extensions/MultiImageTargetGraphics.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Shipwreck {
+	/// <summary>
+	/// Used with MultiImageButton.cs to tint multiple graphics with one button
+	/// (specifically, for the level selection assets)
+	/// Source script: https://forum.unity.com/threads/tint-multiple-targets-with-single-button.279820/
+	/// </summary>
+	public class MultiImageTargetGraphics : MonoBehaviour {
+		[SerializeField] private Graphic[] targetGraphics;
+
+		public Graphic[] GetTargetGraphics => targetGraphics;
+	}
+}
