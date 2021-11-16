14b99b6bd57c5fefb6dc7d2998217d9e5e5504c9
Levi Huillet <levimhuillet@gmail.com>
2021-11-16 15:22:12 -0600

    added close-inspect to family and granmps photos

--- Assets/_Code/UI/UIOfficeScreen.cs
+++ Assets/_Code/UI/UIOfficeScreen.cs
@@ -9,13 +9,23 @@ namespace Shipwreck {
 		[SerializeField]
 		private UIOfficeMapContainer m_officeMapContainer;
 		[SerializeField]
+		private UICloseInspect m_closeInspectScreen;
+		[SerializeField]
 		private Button m_levelMapButton;
+		[SerializeField]
+		private Inspectable[] m_inspectables;
 
 		private void OnEnable() {
 			m_levelMapButton.onClick.AddListener(HandleLevelMapButton);
+			foreach (Inspectable inspectable in m_inspectables) {
+				inspectable.Button.onClick.AddListener(delegate { HandleCloseInspect(inspectable.ID); });
+			}
 		}
 		private void OnDisable() {
 			m_levelMapButton.onClick.RemoveListener(HandleLevelMapButton);
+			foreach (Inspectable inspectable in m_inspectables) {
+				inspectable.Button.onClick.RemoveAllListeners();
+			}
 		}
 
 		protected override void OnShowStart() {
@@ -32,6 +42,10 @@ namespace Shipwreck {
 			AudioSrcMgr.instance.PlayOneShot("click_level");
 			UIMgr.Open<UIMapScreen>();
 			// UIMgr.CloseThenOpen<UIOfficeScreen, UIEvidenceScreen>();
+		}
+
+		private void HandleCloseInspect(string imageID) {
+			m_closeInspectScreen.OpenCloseInspect(imageID);
 		}
 
 		protected override IEnumerator HideRoutine() {
