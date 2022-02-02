41699e773403be20c8698856ad04a4f3aee54692
Levi Huillet <levimhuillet@gmail.com>
2022-02-02 17:26:04 -0600

    corrected missing translation entries and adjusted positioning of certain UI elements

---
+++ Assets/_Code/Sonar/LocalizationAdjuster.cs
@@ -0,0 +1,20 @@
+﻿using PotatoLocalization;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class LocalizationAdjuster : MonoBehaviour
+{
+	[SerializeField]
+	private float m_enWidth, m_esWidth;
+
+    private void Start() {
+        if (LocalizationMgr.CurrentLanguage.Equals(new LanguageCode("en"))) {
+			this.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_enWidth);
+		}
+		else if (LocalizationMgr.CurrentLanguage.Equals(new LanguageCode("es"))) {
+			this.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, m_esWidth);
+		}
+	}
+}
